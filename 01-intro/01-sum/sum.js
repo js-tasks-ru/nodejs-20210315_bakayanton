24551980d30c438729125882afdefd2a2e9bcbc6
Anton Bakay <bakayanton@gmail.com>
2021-03-15 21:07:47 +0200

    01-intro/01-sum/sum.js

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+	/* ваш код */
+	if (typeof a !== 'number' || typeof b !== 'number') {
+		throw new TypeError('Wrong argument type');
+	}
+
+	return a + b;
 }
 
 module.exports = sum;
