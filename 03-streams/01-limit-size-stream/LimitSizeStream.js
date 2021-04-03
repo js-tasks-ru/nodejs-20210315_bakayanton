ec87247616defe06e6b6303df5b9bac1a9993f41
Anton Bakay <bakayanton@gmail.com>
2021-04-03 16:28:06 +0300

    03-streams 01 limit size stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.options = {};
+    this.options.limit = options && options.limit || 0;
+    this.options.encoding = options && options.encoding || 'utf-8';
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += chunk.length;
+    if (this.size > this.options.limit) {
+      callback(new LimitExceededError(), null);
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
